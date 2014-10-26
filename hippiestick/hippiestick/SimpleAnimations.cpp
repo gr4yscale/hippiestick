6d5d5a850508ca992d58f9a7376d033e1986d9d7
Tyler Powers <gr4yscale@gmail.com>
2014-10-26 16:38:39 -0700

    incorporate potentiometer inputs for testing

--- hippiestick/hippiestick/SimpleAnimations.cpp
+++ hippiestick/hippiestick/SimpleAnimations.cpp
@@ -335,49 +335,16 @@ unsigned int SimpleAnimations::microPhoneLevel() {
     }
     peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
     return peakToPeak;
-
-//    for (int ledPosition=0; ledPosition < strip.numPixels(); ledPosition++) {
-//        strip.setPixelColor(ledPosition, strip.Color(0, 0, 0));
-//    }
-//    strip.show();
-//
-//    for (int ledPosition=0; ledPosition < numberOfLights; ledPosition++) {
-//        strip.setPixelColor(ledPosition, color);
-//    }
-//    strip.show();
-
 }
-
-
-
-
 
 void SimpleAnimations::audioReactiveRainbowCycle() {
 
+    // get changes here from another branch
+
     unsigned int micLevel = microPhoneLevel();
-
-    //        double factor = map(analogRead(1), 0, 1024.0, 0, 2.0);
-
-
     double factor = analogRead(1) * 5.0 / 1024;
 
     Serial.println(factor);
-
-    uint16_t i, j;
-
-    for (j=0; j < 384 * 5; j++) {     // 5 cycles of all 384 colors in the wheel
-        for (i=0; i < strip.numPixels(); i++) {
-            // tricky math! we use each pixel as a fraction of the full 384-color
-            // wheel (thats the i / strip.numPixels() part)
-            // Then add in j which makes the colors go around per pixel
-            // the % 384 is to make the wheel cycle around
-            strip.setPixelColor(i, Wheel(((i * 384 / strip.numPixels()) + j) % 384));
-        }
-
-        Serial.println(factor);
-        strip.show();   // write all the pixels out
-    }
-
 }
 
 // private
--- hippiestick/hippiestick/hippiestick.ino
+++ hippiestick/hippiestick/hippiestick.ino
@@ -17,6 +17,13 @@ int g_brightness = 1.0;
 int g_currentMode = 0;
 int g_logHeartbeatInterval = 1000;
 
+int g_paramMicrophoneLevel = 0;
+int g_param1 = 0;
+int g_param2 = 0;
+int g_param3 = 0;
+int g_param4 = 0;
+int g_param5 = 0;
+
 aci_evt_opcode_t ble_laststatus = ACI_EVT_DISCONNECTED;
 
 Adafruit_BLE_UART bleSerial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
@@ -27,7 +34,10 @@ SimpleAnimations modeSimpleAnimations;
 
 void attachCommandCallbacks();
 void pollBluetoothStatus(void);
+void pollMicrophoneLevel();
+void pollPotentiometers();
 void logHeartbeat();
+void logInputs();
 void onUnknownCommand();
 void onSetMode();
 void onSetModeParamSimpleAnimationsColor();
@@ -66,17 +76,16 @@ void setup()
 
 void loop()
 {
+    pollMicrophoneLevel();
+    pollPotentiometers();
+    logInputs();
 
 //    pollBluetoothStatus();
 
 //    modeSimpleAnimations.loop();
-    modeBouncyBallPhysics.loop();
+//    modeBouncyBallPhysics.loop();
 
-
-//    pollBluetoothStatus();
-
-    // wrap looping functions in a wait using CMD_SET_UPDATE_INTERVAL
-
+// wrap looping functions in a wait using CMD_SET_UPDATE_INTERVAL
 
 //    switch (g_currentMode) {
 //        case MODE_COLORFUL_MULTI_BOUNCY_BALL_PHYSICS:
@@ -97,9 +106,8 @@ void loop()
 //            break;
 //    }
 
-    // need a heartbeat
-
 //    logHeartbeat();
+
 }
 
 void attachCommandCallbacks()
@@ -133,6 +141,20 @@ void pollBluetoothStatus(void)
     }
 }
 
+void pollMicrophoneLevel()
+{
+    g_paramMicrophoneLevel = analogRead(0);
+}
+
+void pollPotentiometers()
+{
+    g_param1 = analogRead(1);
+    g_param2 = analogRead(2);
+    g_param3 = analogRead(3);
+    g_param4 = analogRead(4);
+    g_param5 = analogRead(5);
+}
+
 void logHeartbeat()
 {
     static long nextStep = 0;
@@ -142,6 +164,34 @@ void logHeartbeat()
         nextStep += g_logHeartbeatInterval;
         Serial.println("I'm alive!");
     }
+}
+
+void logInputs()
+{
+    Serial.print("param mic level: ");
+    Serial.print(g_paramMicrophoneLevel);
+    Serial.print(" | ");
+
+    Serial.print("param 1: ");
+    Serial.print(g_param1);
+    Serial.print(" | ");
+
+    Serial.print("param 2: ");
+    Serial.print(g_param2);
+    Serial.print(" | ");
+
+    Serial.print("param 3: ");
+    Serial.print(g_param3);
+    Serial.print(" | ");
+
+    Serial.print("param 4: ");
+    Serial.print(g_param4);
+    Serial.print(" | ");
+
+    Serial.print("param 5: ");
+    Serial.print(g_param5);
+
+    Serial.println(" ");
 }
 
 // Command Callbacks
